0733aa2309612cfd5828bd1592b7d0dc7a440124
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2024-03-20 06:24:57 +0300

    Fix service controllers and model

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  before_action :set_service, only: %i[ show edit update destroy ]
+  before_action :set_service, only: %i[show edit update destroy]
 
   # GET /services or /services.json
   def index
@@ -25,7 +25,7 @@ class ServicesController < ApplicationController
 
     respond_to do |format|
       if @service.save
-        format.html { redirect_to service_url(@service), notice: "Service was successfully created." }
+        format.html { redirect_to service_url(@service), notice: 'Service was successfully created.' }
         format.json { render :show, status: :created, location: @service }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ServicesController < ApplicationController
   def update
     respond_to do |format|
       if @service.update(service_params)
-        format.html { redirect_to service_url(@service), notice: "Service was successfully updated." }
+        format.html { redirect_to service_url(@service), notice: 'Service was successfully updated.' }
         format.json { render :show, status: :ok, location: @service }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,21 @@ class ServicesController < ApplicationController
     @service.destroy!
 
     respond_to do |format|
-      format.html { redirect_to services_url, notice: "Service was successfully destroyed." }
+      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_service
-      @service = Service.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def service_params
-      params.require(:service).permit(:status, :start_time, :duration, :type, :description, :cost, :car_id, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def service_params
+    params.require(:service).permit(:status, :start_time, :duration, :service_type, :description, :cost, :car_id,
+                                    :employee_id)
+  end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,10 @@
 class Service < ApplicationRecord
   belongs_to :car
-  belongs_to :employee
+  belongs_to :employee, optional: true
 
+  alias_attribute :employee_id, :user_id
   attribute :duration, :duration
+
+  enum status: %i[booked pending started complete cancelled]
+  enum service_type: %i[minor major custom]
 end

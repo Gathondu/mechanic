23d6fd1e3ace7b8438afea6464e1a73a7763dee3
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2024-04-02 12:10:28 +0300

    show only cars belonging to a customer

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,7 +3,7 @@ class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
 
   def index
-    @cars = Car.all
+    @cars = @customer.cars
   end
 
   def show

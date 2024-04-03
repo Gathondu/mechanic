bee37f5ade6f7435c7a626059b1e5f433af72627
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2024-04-03 07:38:52 +0300

    add token flow for forgot password

--- app/helpers/passwords_helper.rb
+++ app/helpers/passwords_helper.rb
@@ -1,2 +1,6 @@
 module PasswordsHelper
+  def user_type
+    # using this for now to parse the user type param sent via url to avoid the amp; appended to the param
+    CGI.parse(URI.parse(request.url).query).fetch('type').first
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :cars, dependent: :destroy
+
   TYPE_NAMES = %w[Admin Customer Employee].freeze
+
   validates :type, inclusion: { in: TYPE_NAMES, message: "User must be one of: #{TYPE_NAMES.join(', ')}" }
   validates :email, presence: true, uniqueness: true
   normalizes :email, with: ->(email) { email.strip.downcase }
 
-  has_many :cars, dependent: :destroy
+  generates_token_for :password_reset, expires_in: 15.minutes do
+    password_salt&.last(10)
+  end
 end

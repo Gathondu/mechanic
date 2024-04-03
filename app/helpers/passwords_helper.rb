09f0b25a00fd62437365c0f77c821584230449dc
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2024-04-03 11:08:04 +0300

    add text version of password reset mailer

--- app/helpers/passwords_helper.rb
+++ app/helpers/passwords_helper.rb
@@ -1,6 +1,7 @@
 module PasswordsHelper
   def user_type
     # using this for now to parse the user type param sent via url to avoid the amp; appended to the param
+    # This is until we set up the mailer
     CGI.parse(URI.parse(request.url).query).fetch('type').first
   end
 end
